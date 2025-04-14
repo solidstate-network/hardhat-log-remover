6a06da94805a3d3ee25a8761041c3d4cad16e665
Nick Barry <itsnickbarry@protonmail.ch>
2025-04-14 15:38:17 -0600

    export HardhatPlugin object and register in test configuration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
+import pkg from '../package.json';
 import './tasks/remove_logs';
+import { HardhatPlugin } from 'hardhat/types/plugins';
+
+const plugin: HardhatPlugin = {
+  id: pkg.name.split('/').pop()!,
+  npmPackage: pkg.name!,
+};
+
+export default plugin;
