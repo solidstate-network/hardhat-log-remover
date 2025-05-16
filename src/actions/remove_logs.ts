7d32f70b1519842684f2c44ebd3a922344a6169a
Nick Barry <itsnickbarry@protonmail.ch>
2025-05-15 23:52:34 -0600

    import NewTaskActionFunction as type

--- src/actions/remove_logs.ts
+++ src/actions/remove_logs.ts
@@ -1,7 +1,7 @@
 import pkg from '../../package.json';
 import { removeLogs } from '../lib/log_remover.js';
 import { HardhatPluginError } from 'hardhat/plugins';
-import { NewTaskActionFunction } from 'hardhat/types/tasks';
+import type { NewTaskActionFunction } from 'hardhat/types/tasks';
 
 const action: NewTaskActionFunction = async (args, hre) => {
   try {
