79880042e3025ac5964e9007e940c76c42f561e1
Nick Barry <itsnickbarry@protonmail.ch>
2025-05-20 14:17:29 -0600

    remove hardhat-node-test-runner

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,8 @@
 import HardhatLogRemover from './src/index.js';
-import HardhatNodeTestRunner from '@nomicfoundation/hardhat-node-test-runner';
 import type { HardhatUserConfig } from 'hardhat/config';
 
 const config: HardhatUserConfig = {
-  plugins: [HardhatNodeTestRunner, HardhatLogRemover],
+  plugins: [HardhatLogRemover],
 };
 
 export default config;
